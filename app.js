80c3d56b451b6e79576276395be089876f6c4046
avenal <avenalszarosiwy@gmail.com>
2018-12-30 19:49:26 +0100

    adding tasks to list

---
+++ app.js
@@ -0,0 +1,31 @@
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+
+loadEventListeners();
+
+function loadEventListeners(){
+    form.addEventListener('submit', addTask);
+}
+
+function addTask(e){
+    if(taskInput.value==='')
+    {
+        M.toast({html: 'Add a task'})
+    }
+    else
+    {
+        const li = document.createElement('li');
+        li.className = 'collection-item';
+        li.appendChild(document.createTextNode(taskInput.value));
+        const link = document.createElement('a');
+        link.className = 'delete-item secondary-content';
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        li.appendChild(link);
+        taskList.appendChild(li);
+    }
+    e.preventDefault();
+}
